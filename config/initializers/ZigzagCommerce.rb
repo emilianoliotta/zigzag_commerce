d56d289f9b1ed91f50ff1cab772184f45bbc084e
Emiliano Liotta <liotta.emiliano@gmail.com>
2015-11-15 16:43:36 -0300

    ZigzagCommerce.rb updated.

--- config/initializers/ZigzagCommerce.rb
+++ config/initializers/ZigzagCommerce.rb
@@ -1,13 +1,13 @@
 # Configuración de cliente de MercadoPago
 	# Credenciales de cliente
 
-		CLIENT_ID = "Mp.find(1).client_id.to_s"
-		CLIENT_SECRET = "MpKey.find(1).client_secret.to_s"
+		CLIENT_ID = Mp.find(1).client_id.to_s
+		CLIENT_SECRET = Mp.find(1).client_secret.to_s
 
 	# Credenciales de modo Sandbox
 
-		PUBLIC_KEY = "MpKey.find(1).public_key.to_s"
-		ACCESS_TOKEN = "MpKey.find(1).access_token.to_s"
+		PUBLIC_KEY = Mp.find(1).public_key.to_s
+		ACCESS_TOKEN = Mp.find(1).access_token.to_s
 
 	# Cliente de MercadoPagorai
 
